f6b3a14f1174d57eb9840eb3d06ca6bbae25bc03
Jappowski <58007440+Jappowski@users.noreply.github.com>
2020-04-23 10:32:54 +0200

    .

--- FormCreator/script.js
+++ FormCreator/script.js
@@ -1,30 +1,66 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var FieldType;
 (function (FieldType) {
     FieldType[FieldType["textBox"] = 1] = "textBox";
     FieldType[FieldType["textArea"] = 2] = "textArea";
-    FieldType[FieldType["data"] = 3] = "data";
+    FieldType[FieldType["date"] = 3] = "date";
     FieldType[FieldType["email"] = 4] = "email";
     FieldType[FieldType["radio"] = 5] = "radio";
     FieldType[FieldType["checkbox"] = 6] = "checkbox";
 })(FieldType || (FieldType = {}));
-var InputField = (function () {
+var app = (function () {
+    function app() {
+    }
+    app.prototype.createInput = function (name) {
+        var para = document.createElement("p");
+        var node = document.createTextNode(name);
+        var inputnode = document.createElement("input");
+        para.appendChild(node);
+        para.appendChild(inputnode);
+        var element = document.getElementById("div1");
+        element.appendChild(para);
+        return para;
+    };
+    app.prototype.createCheckbox = function (name) {
+        var para = document.createElement("p");
+        var node = document.createTextNode(name);
+        var checknode = document.createElement("input");
+        checknode.type = "checkbox";
+        para.appendChild(node);
+        para.appendChild(checknode);
+        var element = document.getElementById("div1");
+        element.appendChild(para);
+        return para;
+    };
+    return app;
+})();
+var InputField = (function (_super) {
+    __extends(InputField, _super);
     function InputField(name) {
+        _super.call(this);
         this.element = document.createElement('input');
         this.name = name;
         this.label = "etykieta";
         this.element.name = this.name;
         this.type = FieldType.textBox;
+        this.value = "";
     }
     InputField.prototype.render = function () {
-        return this.element;
+        return this.createInput(this.name);
     };
     InputField.prototype.getValue = function () {
         return this.element.value;
     };
     return InputField;
-})();
-var TextArea = (function () {
+})(app);
+var TextArea = (function (_super) {
+    __extends(TextArea, _super);
     function TextArea(name) {
+        _super.call(this);
         this.element = document.createElement('input');
         this.name = name;
         this.label = "etykieta";
@@ -32,29 +68,49 @@ var TextArea = (function () {
         this.type = FieldType.textArea;
     }
     TextArea.prototype.render = function () {
-        return this.element;
+        return this.createInput(this.name);
     };
     TextArea.prototype.getValue = function () {
         return this.element.value;
     };
     return TextArea;
-})();
-var DateField = (function () {
+})(app);
+var DateField = (function (_super) {
+    __extends(DateField, _super);
     function DateField(name) {
+        _super.call(this);
         this.element = document.createElement('date');
         this.name = name;
         this.label = "etykieta";
         this.element.name = this.name;
-        this.type = FieldType.data;
+        this.type = FieldType.date;
     }
     DateField.prototype.render = function () {
-        return this.element;
+        return this.createInput(this.name);
     };
     DateField.prototype.getValue = function () {
         return this.element.value;
     };
     return DateField;
-})();
+})(app);
+var CheckField = (function (_super) {
+    __extends(CheckField, _super);
+    function CheckField(name) {
+        _super.call(this);
+        this.element = document.createElement('checkbox');
+        this.name = name;
+        this.label = "etykieta";
+        this.element.name = this.name;
+        this.type = FieldType.checkbox;
+    }
+    CheckField.prototype.render = function () {
+        return this.createCheckbox(this.name);
+    };
+    CheckField.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return CheckField;
+})(app);
 var Form = (function () {
     function Form(id) {
         this.fields = new Array();
@@ -68,12 +124,14 @@ var Form = (function () {
     Form.prototype.render = function () {
         var inputField = new InputField("Imie");
         inputField.render();
+        this.fields.push(inputField);
         var textArea = new TextArea("Nazwisko");
         textArea.render();
         var dataArea = new DateField("Data");
         dataArea.render();
+        var checkArea = new CheckField("Kromka");
+        checkArea.render();
     };
     return Form;
 })();
-var app = new Form("1");
-app.render();
+var ap = new Form("1");
--- FormCreator/script.ts
+++ FormCreator/script.ts
@@ -1,7 +1,7 @@
 enum FieldType {
     textBox = 1,
     textArea = 2,
-    data = 3,
+    date = 3,
     email = 4,
     radio = 5,
     checkbox = 6,
@@ -15,35 +15,64 @@ interface Field{
     getValue(): any;
 }
 
-
-class InputField implements Field{
+class app{
+    
+    createInput(name:string){
+        var para = document.createElement("p")
+        var node = document.createTextNode(name)
+        var inputnode = document.createElement("input")
+        para.appendChild(node)
+        para.appendChild(inputnode)
+        var element = document.getElementById("div1")
+        element.appendChild(para)
+        return para;
+        
+    }
+    createCheckbox(name:string){
+        var para = document.createElement("p")
+        var node = document.createTextNode(name)
+        var checknode = document.createElement("input")
+        checknode.type = "checkbox"
+        para.appendChild(node)
+        para.appendChild(checknode)
+        
+        var element = document.getElementById("div1")
+        element.appendChild(para)
+        return para;
+        
+    }
+}
+class InputField extends app implements Field{
     name: string;
     label: string;
     type: FieldType;
     element: HTMLInputElement;
     value: any;
     constructor(name: string){
-        
+        super();
         this.element = <HTMLInputElement>document.createElement('input');
         this.name = name;
         this.label = "etykieta";
         this.element.name = this.name;
         this.type = FieldType.textBox;
+        this.value= "";
     }
     render(): HTMLElement {
-       return this.element;
+       return this.createInput(this.name);
     }
     getValue(): any{
-        return this.element.value
+     return this.element.value
+        
     }
 }
-class TextArea  implements Field{
+class TextArea extends app implements Field{
     name: string;
     label: string;
     type: FieldType;
     element: HTMLInputElement;
     value: any;
     constructor(name: string){
+        super();
         this.element = <HTMLInputElement>document.createElement('input');
         this.name = name;
         this.label = "etykieta";
@@ -51,27 +80,51 @@ class TextArea  implements Field{
         this.type = FieldType.textArea;
     }
     render(): HTMLElement {
-       return this.element
+       return this.createInput(this.name)
     }
     getValue(): any{
         return this.element.value
     }
 }
-class DateField implements Field{
+class DateField extends app implements Field{
     name: string;
     label: string;
     type: FieldType;
     element: HTMLInputElement;
     
     constructor(name: string){
+        super();
         this.element = <HTMLInputElement>document.createElement('date');
         this.name = name;
         this.label = "etykieta";
         this.element.name = this.name;
-        this.type = FieldType.data;
+        this.type = FieldType.date;
     }
-    render(): HTMLElement {
-       return this.element;
+    render():HTMLElement {
+        return this.createInput(this.name);
+    }
+    getValue(): any{
+        return this.element.value
+    }
+}
+
+class CheckField extends app implements Field{
+    name: string;
+    label: string;
+    type: FieldType;
+    element: HTMLInputElement;
+    
+    constructor(name: string){
+        super();
+        this.element = <HTMLInputElement>document.createElement('checkbox');
+        this.name = name;
+        this.label = "etykieta";
+        this.element.name = this.name;
+        this.type = FieldType.checkbox;
+    }
+    render():HTMLElement {
+      return this.createCheckbox(this.name);
+      
     }
     getValue(): any{
         return this.element.value
@@ -87,26 +140,28 @@ class Form {
         this.formElement = document.getElementById(id);
     }
     getValue(): void{
-        this.fields.forEach(element => {
+       this.fields.forEach(element => {
             element.getValue();
         });
-        
+       
     }
     render() {
           let inputField: InputField = new InputField("Imie");
           inputField.render();
-          
+          this.fields.push(inputField);
 
           let textArea: TextArea = new TextArea("Nazwisko");
           textArea.render();
-          
+         
           
           let dataArea: DateField = new DateField("Data");
           dataArea.render();
-          
+         
+          let checkArea: CheckField = new CheckField("Kromka")
+          checkArea.render();
           
     }
 }
 
-let app = new Form("1");
-app.render();
+let ap = new Form("1");
+
